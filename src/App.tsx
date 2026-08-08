3576b59003c4c5b304baa365cb4e3a890c2726ce
Pierre Pontet <ppontet@student.42lyon.fr>
2026-08-08 16:22:25 +0200

    feat: darkMode theme with toggle button

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+import { Suspense } from 'react'
 import { RouterProvider } from 'react-router-dom'
 import { router } from '@/utils/router'
 import TopNavBar from './components/TopNavBar'
+import { ThemeProvider } from "@/components/theme-provider"
 
 export default function App() {
-	return (
-	<>
-		<TopNavBar />
-		<RouterProvider router={router} />
-	</>
-	)
-}
+	return(
+		<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+			<Suspense fallback={null}>
+				<TopNavBar />
+				<RouterProvider router={router} />
+			</Suspense>
+		</ThemeProvider>
+)}
--- src/pages/file/index.tsx
+++ src/pages/file/index.tsx
@@ -1,5 +1,11 @@
 import FileListWidget from '@/components/FileListWidget'
+import { ModeToggle } from '@/components/mode-toggle'
 
 export default function IndexPage() {
-	return <FileListWidget mode="full" showPagination={true} showUpload={true} initialLimit={10} />
+	return(
+		<>
+			<ModeToggle />
+			<FileListWidget mode="full" showPagination={true} showUpload={true} initialLimit={10} />
+		</>
+	)
 }
