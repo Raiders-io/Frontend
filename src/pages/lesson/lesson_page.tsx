7ffe19a9807f68ef4610bc2a8fd2a1ead2ebfbcb
Pierre Pontet <ppontet@student.42lyon.fr>
2026-08-09 15:54:03 +0200

    feat: poc for resizable Zone

--- src/pages/lesson/lesson_page.tsx
+++ src/pages/lesson/lesson_page.tsx
@@ -1,6 +1,37 @@
-// import React from "react";
-// import { input, button } from "@/components/ui";
-// import { lessonService } from "@/services/lesson_service";
+import FileListWidget from '@/components/FileListWidget'
+import { Button } from '@/components/ui/button'
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable"
+import { useState } from 'react'
 
-// export default function LessonPage() {
-// }
+export default function IndexPage() {
+    const [zoneOneVisible, setZoneOneVisible] = useState(true)
+    const [zoneTwoVisible, setZoneTwoVisible] = useState(true)
+
+    return (
+<>
+    <div>
+      <Button onClick={() => setZoneOneVisible(!zoneOneVisible)}>{zoneOneVisible ? 'Hide One' : 'Show One'}</Button>
+      <Button onClick={() => setZoneTwoVisible(!zoneTwoVisible)}>{zoneTwoVisible ? 'Hide Two' : 'Show Two'}</Button>
+    </div>
+    <ResizablePanelGroup orientation="horizontal">
+    {zoneOneVisible && (
+      <ResizablePanel>
+        Zone One
+        <FileListWidget mode="full" showPagination={true} showUpload={true} initialLimit={10} />
+      </ResizablePanel>
+    )}
+    <ResizableHandle />
+    {zoneTwoVisible && (
+      <ResizablePanel>
+        Zone Two
+        <FileListWidget mode="full" showPagination={true} showUpload={true} initialLimit={10} />
+      </ResizablePanel>
+    )}
+  </ResizablePanelGroup>
+</>
+    )
+}
