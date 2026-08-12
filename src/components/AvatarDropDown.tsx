60611f7cb7fbc82987da31ad7ffe6c6f178f7137
Pierre Pontet <ppontet@student.42lyon.fr>
2026-08-12 17:06:29 +0200

    feat: log out button uses alert dialog

--- src/components/AvatarDropDown.tsx
+++ src/components/AvatarDropDown.tsx
@@ -22,6 +22,7 @@ import {
   UsersIcon,
   MessageCircleIcon,
 } from "lucide-react"
+import { AlertDialog, AlertDialogAction, AlertDialogCancel, AlertDialogContent, AlertDialogDescription, AlertDialogFooter, AlertDialogHeader, AlertDialogMedia, AlertDialogTitle, AlertDialogTrigger } from "@/components/ui/alert-dialog"
 
 export function AvatarDropdown() {
   const { user, logout } = useAuthStore()
@@ -97,12 +98,36 @@ export function AvatarDropdown() {
         </DropdownMenuGroup>
         <DropdownMenuSeparator />
         <DropdownMenuGroup>
-          <DropdownMenuItem variant="destructive" onClick={handleLogout}>
-            <LogOutIcon />
-            Log out
-          </DropdownMenuItem>
+          <LogOutButton action={handleLogout} />
         </DropdownMenuGroup>
       </DropdownMenuContent>
     </DropdownMenu>
   )
 }
+
+const LogOutButton = ({ action }: { action: () => void }) => {
+  return (
+  <AlertDialog>
+    <AlertDialogTrigger asChild>
+      <DropdownMenuItem variant="destructive" onSelect={(e) => e.preventDefault()} className="cursor-pointer">
+          <LogOutIcon />
+          Log out
+        </DropdownMenuItem>
+    </AlertDialogTrigger>
+    <AlertDialogContent size="sm">
+      <AlertDialogHeader>
+        <AlertDialogMedia className="bg-destructive/10 text-destructive dark:bg-destructive/20 dark:text-destructive">
+          <LogOutIcon />
+        </AlertDialogMedia>
+        <AlertDialogTitle>Log out</AlertDialogTitle>
+        <AlertDialogDescription>
+          You will get disconnected
+        </AlertDialogDescription>
+      </AlertDialogHeader>
+      <AlertDialogFooter>
+        <AlertDialogCancel variant="outline">Cancel</AlertDialogCancel>
+        <AlertDialogAction variant="destructive" onClick={action}>Log out</AlertDialogAction>
+      </AlertDialogFooter>
+    </AlertDialogContent>
+  </AlertDialog>
+)}
