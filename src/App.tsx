a49a367cee437972a6719f842e2c98f4b7675ff4
Pierre Pontet <ppontet@student.42lyon.fr>
2026-08-12 16:45:55 +0200

    fix: default theme is system instead of dark

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { ThemeProvider } from "@/components/theme-provider"
 
 export default function App() {
 	return(
-		<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+		<ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
 			<Suspense fallback={null}>
 				<TopNavBar />
 				<RouterProvider router={router} />
