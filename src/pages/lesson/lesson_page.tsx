05a810685a09a17db5a9677f2ed5eb648f57886b
Pierre Pontet <ppontet@student.42lyon.fr>
2026-08-12 11:19:13 +0200

    feat: not showing hide button and separator if one zone is hidden

--- src/pages/lesson/lesson_page.tsx
+++ src/pages/lesson/lesson_page.tsx
@@ -5,6 +5,7 @@ import {
   ResizablePanel,
   ResizablePanelGroup,
 } from "@/components/ui/resizable"
+import { PanelLeftClose, PanelLeftOpen, PanelRightClose, PanelRightOpen } from 'lucide-react'
 import { useState } from 'react'
 
 export default function IndexPage() {
@@ -13,18 +14,25 @@ export default function IndexPage() {
 
     return (
 <>
-    <div>
-      <Button onClick={() => setZoneOneVisible(!zoneOneVisible)}>{zoneOneVisible ? 'Hide One' : 'Show One'}</Button>
-      <Button onClick={() => setZoneTwoVisible(!zoneTwoVisible)}>{zoneTwoVisible ? 'Hide Two' : 'Show Two'}</Button>
-    </div>
+     
     <ResizablePanelGroup orientation="horizontal">
+    {!zoneTwoVisible ? "" :(
+      <Button onClick={() => setZoneOneVisible(!zoneOneVisible)}>
+        {zoneOneVisible ? <PanelLeftClose /> : <PanelLeftOpen />}
+      </Button>
+    )}
     {zoneOneVisible && (
       <ResizablePanel>
         Zone One
         <FileListWidget mode="full" showPagination={true} showUpload={true} initialLimit={10} />
       </ResizablePanel>
     )}
-    <ResizableHandle />
+    {!zoneOneVisible || !zoneTwoVisible ? null : <ResizableHandle />}
+    {!zoneOneVisible ? "" :(
+      <Button onClick={() => setZoneTwoVisible(!zoneTwoVisible)}>
+        {zoneTwoVisible ? <PanelRightClose /> : <PanelRightOpen />}
+      </Button>
+    )}
     {zoneTwoVisible && (
       <ResizablePanel>
         Zone Two
