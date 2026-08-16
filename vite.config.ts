16e807c5cde2273fcbe0567f29ba1de8afbe5183
Pierre Pontet <ppontet@student.42lyon.fr>
2026-08-16 14:43:02 +0200

    chores: specify sourcemap gen as false and update manualChunks (deprecated) to codeSplitting

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  	plugins: [
-		react(),
-		tailwindcss(),
-	],
-	build: {
-		rollupOptions: {
-			output: {
-				manualChunks(id) {
-					if (!id.includes('node_modules')) return
-
-					if (id.includes('react-router')) return 'router'
-					if (id.includes('react-hook-form') || id.includes('zod')) return 'forms'
-					if (id.includes('lucide-react')) return 'icons'
-					if (id.includes('@radix-ui')) return 'radix'
-
-					return 'vendor'
-				},
-			},
-		},
-	},
-	resolve: {
-		alias: {
-			'@': path.resolve(__dirname, './src'),
-		},
-	},
+  plugins: [react(), tailwindcss()],
+  build: {
+    sourcemap: false, // True if you want source maps for debugging
+    rollupOptions: {
+      output: {
+        codeSplitting: {
+          groups: [
+            {
+              name(id) {
+                if (!id.includes("node_modules")) return
+                if (id.includes("react-router")) return "router"
+                if (id.includes("react-hook-form") || id.includes("zod"))
+                  return "forms"
+                if (id.includes("lucide-react")) return "icons"
+                if (id.includes("@radix-ui")) return "radix"
+                return "vendor"
+              },
+            },
+          ],
+        },
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
