f3f7504cb00bdc9c4062b14554693269c0261f36
Pierre Pontet <ppontet@student.42lyon.fr>
2026-08-20 20:45:28 +0200

    feat: better footer

--- src/components/FooterComponent.tsx
+++ src/components/FooterComponent.tsx
@@ -1,4 +1,10 @@
-import { Card } from "@/components/ui/card"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 import {
   Accordion,
   AccordionItem,
@@ -17,34 +23,15 @@ import {
   changePageAbout,
   changePageContact,
 } from "@/utils/router/changePage"
-import { Separator } from "@/components/ui/separator"
-
-const NavButton = ({
-  label,
-  onClick,
-}: {
-  label: string
-  onClick?: () => void
-}) => {
-  return (
-    <Button
-      variant="ghost"
-      className="items-center justify-center w-20 h-[30%]"
-      onClick={onClick}
-    >
-      {label}
-    </Button>
-  )
-}
 
 export const FooterComponent = () => {
-  return (
-    <footer>
-      <Card className="flex h-64 w-full flex-row gap-4 p-4">
-        <Accordion type="single" collapsible className="w-100 p-4">
+  const AccordionFooter = () => {
+    return (
+      <div className="min-w-0 flex-1 overflow-hidden">
+        <Accordion type="single" collapsible className="h-full p-4">
           <AccordionItem value="item-1">
             <AccordionTrigger>Quick info 1</AccordionTrigger>
-            <AccordionContent>Content of the first ite²m.</AccordionContent>
+            <AccordionContent>Content of the first item.</AccordionContent>
           </AccordionItem>
           <AccordionItem value="item-2">
             <AccordionTrigger>Quick info 2</AccordionTrigger>
@@ -59,35 +46,91 @@ export const FooterComponent = () => {
             <AccordionContent>Content of the fourth item.</AccordionContent>
           </AccordionItem>
         </Accordion>
-        <div className="flex h-full w-[80%] flex-col flex-wrap items-center justify-center gap-2">
-          <NavButton label="Home" onClick={changePageHome} />
-          <NavButton label="Files" onClick={changePageFileList} />
-          <NavButton label="Lessons" onClick={changePageLessons} />
-          <Separator orientation="vertical" className="h-[80%] border-1" />
-          <NavButton label="Profile" onClick={changePageProfile} />
-          <NavButton label="Friends" onClick={changePageFriends} />
-          <NavButton label="Messages" onClick={changePageChat} />
-          <Separator orientation="vertical" className="h-[80%] border-1" />
-          <NavButton label="Settings" onClick={changePageEditProfile} />
-          <NavButton label="About" onClick={changePageAbout} />
-          <NavButton label="Contact" onClick={changePageContact} />
+      </div>
+    )
+  }
+
+  const NavLinks = () => {
+    const NavButton = ({
+      label,
+      onClick,
+    }: {
+      label: string
+      onClick?: () => void
+    }) => {
+      return (
+        <Button variant="ghost" className="h-[30%] w-full" onClick={onClick}>
+          {label}
+        </Button>
+      )
+    }
+
+    const CustomSeparator = () => {
+      return (
+        <div className="flex items-center justify-center">
+          <div className="h-[50%] w-full max-w-[1px] border-1 border-dashed"></div>
         </div>
-        <div className="flex h-full w-50 shrink-0 flex-col items-center justify-between">
-          <Button
-            variant="ghost"
-            className="flex h-full w-full flex-col items-center justify-center gap-2 rounded-lg border p-4 text-lg font-semibold"
-            onClick={changePageHome}
-          >
-            <img
-              src="/favicon.png"
-              alt="Logo"
-              className="h-[50%] w-full rounded-full p-0 object-contain rounded-lg"
-            />
-            <span className="h-[20%] w-full px-4 text-lg font-semibold">
-              Raiders.io
-            </span>
-          </Button>
+      )
+    }
+
+    return (
+      <div className="min-w-0 flex-1 h-full overflow-x-auto">
+        <div className="h-full grid sm:grid-cols-5 grid-cols-5 gap-2 min-w-[320px] sm:min-w-0 shrink-0">
+          <div className="min-w-0 flex flex-col items-center gap-2 justify-center">
+            <NavButton label="Home" onClick={changePageHome} />
+            <NavButton label="Files" onClick={changePageFileList} />
+            <NavButton label="Lessons" onClick={changePageLessons} />
+          </div>
+          <CustomSeparator />
+          <div className="min-w-0 flex flex-col items-center gap-2 justify-center">
+            <NavButton label="Profile" onClick={changePageProfile} />
+            <NavButton label="Friends" onClick={changePageFriends} />
+            <NavButton label="Messages" onClick={changePageChat} />
+          </div>
+          <CustomSeparator />
+          <div className="min-w-0 flex flex-col items-center gap-2 justify-center">
+            <NavButton label="Settings" onClick={changePageEditProfile} />
+            <NavButton label="About" onClick={changePageAbout} />
+            <NavButton label="Contact" onClick={changePageContact} />
+          </div>
         </div>
+      </div>
+    )
+  }
+
+  return (
+    <footer className="position-sticky bottom-0 z-50 w-full border-t bg-background/80 backdrop-blur">
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-lg font-semibold text-center m-0 p-0 gap-0">
+            Project Transcendence
+          </CardTitle>
+          <CardDescription className="text-sm text-center m-0 p-0 gap-0">
+            A project by Raiders.io
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="flex flex-col sm:flex-row gap-4 overflow-hidden p-4 sm:min-h-64 sm:max-h-100 w-full">
+          <AccordionFooter />
+          <div className="margin-auto min-w-0 flex-1">
+            <NavLinks />
+          </div>
+          <div className="min-w-0 flex-1 flex h-full flex-col items-center justify-center">
+            <Button
+              variant="ghost"
+              className="flex flex-col items-center justify-center gap-2 rounded-lg border p-2 text-lg font-semibold md:h-[80%] md:w-[80%] sm:h-[400px] sm:w-[400px] h-[400px] w-[400px]"
+              onClick={changePageHome}
+            >
+              <img
+                src="/favicon.png"
+                alt="Logo"
+                className="h-[50%] w-full rounded-lg object-contain"
+              />
+              <span className="h-[20%] w-full px-2 text-lg font-semibold truncate">
+                Raiders.io
+              </span>
+            </Button>
+          </div>
+        </CardContent>
       </Card>
     </footer>
   )
