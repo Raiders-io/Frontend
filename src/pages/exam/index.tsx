031504e369eb0d412fb74c53ff59142e1ee9d2e6
Yazid Bokhari <yabokhar@z1r5p4.42lyon.fr>
2026-08-31 17:52:24 +0200

    fix: normalize imports + replaced by components

---
+++ src/pages/exam/index.tsx
@@ -0,0 +1,51 @@
+import { createBrowserRouter } from 'react-router-dom'
+import LoginPage from '@/pages/auth/login'
+import SignupPage from '@/pages/auth/signup'
+import HomePage from '@/pages/home'
+import ChatPage from '@/pages/services/chat/chat_page'
+import ProtectedRoute from '@/utils/router/protected_route'
+import LessonPage from '@/pages/lesson/lesson_home_page'
+import LessonHomePage from '@/pages/lesson/lesson_home_page'
+import FileListPage from '@/pages/file/index'
+// import Question from '../../pages/exam/Question'
+//import path from 'path'
+import ExamAuthoring from '@/pages/exam/ExamAuthoring'
+
+export const router = createBrowserRouter([
+	{
+		path: '/',
+		element: (
+			<ProtectedRoute>
+				<HomePage />
+			</ProtectedRoute>
+		),
+	},
+	{
+		path: '/login',
+		element: <LoginPage />,
+	},
+	{
+		path: '/signup',
+		element: <SignupPage />,
+	},
+	{
+		path: '/lessons/',
+		element: <LessonHomePage />,
+	},
+	{
+		path: '/lessons/:slug',
+		element: <LessonPage />,
+	},
+	{
+		path: '/file/list',
+		element: <FileListPage />,
+	},
+	{
+		path: '/chat',
+		element: <ChatPage />,
+	},
+	{
+		path: '/exam/authoring',
+		element: <ExamAuthoring />,
+	},
+])
