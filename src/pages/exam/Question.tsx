cb95b322c2f06d238862a377297dee5769a61fcd
Yazid Bokhari <yabokhar@z1r5p4.42lyon.fr>
2026-08-31 18:19:52 +0200

    Removed true/false option value in Question.tsx

--- src/pages/exam/Question.tsx
+++ src/pages/exam/Question.tsx
@@ -14,7 +14,6 @@ export function CreateQuestion() {
             <select className="border rounded-md p-2">
                 <option value="multiple_choice">QCM</option>
                 <option value="exact_answer">Réponse exacte</option>
-                <option value="true_false">Vrai / Faux</option>
             </select>
 
             <Input placeholder="Answer" />
