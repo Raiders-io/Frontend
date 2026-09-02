ddb5a76c35306ddf7477ffd0008575001041246f
Pierre Pontet <ppontet@student.42lyon.fr>
2026-09-02 14:03:43 +0200

    Imported local work: beginning of front

---
+++ src/pages/exam/ExamAuthoring.tsx
@@ -0,0 +1,25 @@
+import { Button } from "../../components/ui/button"
+import { Input } from "../../components/ui/input"
+import { CreateQuestion } from "./Question"
+
+export default function ExamAuthoring()
+{
+    return (
+    <>
+    <h1>Exam Authoring</h1>
+    {createExam()}
+    {CreateQuestion()}
+    </>
+    )
+}
+
+export function createExam()
+{
+    return (
+        <div className="flex flex-col gap-4">
+            <Input placeholder="Exam Name" />
+            <Input placeholder="Exam Description" />
+            <Button>Submit</Button>
+        </div>
+    )
+}
