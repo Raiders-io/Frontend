4f9dff8c6ea8cbc0d6c7aefc2eb6bc150ee620d5
Yazid Bokhari <yabokhar@z3r2p4.42lyon.fr>
2026-09-16 14:45:22 +0200

    Fix: path for cn

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import { cva, type VariantProps } from "class-variance-authority"
-import { cn } from "cn"
+import { cn } from "@/utils/lib/shadcn"
 import { Slot } from "radix-ui"
 
 const badgeVariants = cva(
--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { cn } from "cn"
+import { cn } from "@/utils/lib/shadcn"
 
 function Textarea({ className, ...props }: React.ComponentProps<"textarea">) {
   return (
